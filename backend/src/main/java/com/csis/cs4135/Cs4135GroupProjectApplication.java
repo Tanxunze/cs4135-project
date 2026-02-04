8b918e7fce99784ceedbf4be9ff171a3df2b3b25
Xunze Tan <24247472@studentmail.ul.ie>
2026-02-04 20:57:52 +0000

    basic structure & config frozen

---
+++ backend/src/main/java/com/csis/cs4135/Cs4135GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.csis.cs4135;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4135GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4135GroupProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/csis/cs4135/Cs4135GroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis.cs4135;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4135GroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
